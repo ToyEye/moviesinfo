2dced35363c1832087a4c7d1ddef1b1edaef7692
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2023-07-21 14:23:35 +0300

    add search

--- src/components/CatalogMovies/CatalogMovies.jsx
+++ src/components/CatalogMovies/CatalogMovies.jsx
@@ -3,10 +3,12 @@ import MovieList from 'components/MovieList';
 import Container from 'components/Container/Container.styled';
 import { Section } from './CatalogMovies.styled';
 
-const CatalogMovies = () => {
+const CatalogMovies = ({ movies }) => {
   return (
     <Section>
-      <Container></Container>
+      <Container>
+        <MovieList movies={movies} />
+      </Container>
     </Section>
   );
 };
