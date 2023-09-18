065626837cc9c96dd7cbc091108d566711328bef
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2023-09-18 19:20:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter>
+        <BrowserRouter basename="/movies">
           <App />
         </BrowserRouter>
       </PersistGate>
